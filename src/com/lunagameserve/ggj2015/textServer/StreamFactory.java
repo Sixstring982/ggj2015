96d1df7ab0a7f01700418fef1801b6c05ea0959f
Trent Small <sixstring982@gmail.com>
2015-01-24 10:18:16 -0700

    Added StreamFactory

---
+++ src/com/lunagameserve/ggj2015/textServer/StreamFactory.java
@@ -0,0 +1,15 @@
+package com.lunagameserve.ggj2015.textServer;
+
+/**
+ * Created by Six on 1/24/2015.
+ */
+public class StreamFactory {
+
+    /**
+     * Creates an instance of a {@link Stream}.
+     * @return
+     */
+    public static Stream createStream() {
+        return null;
+    }
+}
