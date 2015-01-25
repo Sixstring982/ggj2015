2c1219ded91e841d985fcef6680fcc61821ca558
Glenn Eguchi <glenn@laulaulabs.com>
2015-01-24 21:00:25 -0700

    dummy config

---
+++ src/com/lunagameserve/ggj2015/textServer/TextServerConfig.java
@@ -0,0 +1,10 @@
+package com.lunagameserve.ggj2015.textServer;
+
+/**
+ * Created by glenn on 1/24/15.
+ */
+public class TextServerConfig {
+    public static String TwilioUsername = "ASK GLENN";
+    public static String TwilioPassword = "ASK GLENN";
+    public static String TwilioPhoneNumber = "+14087695750";
+}
